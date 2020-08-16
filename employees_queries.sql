f767d2a2ea45d62155a71a3879fe90387e62df56
Austen Rhyce Erickson <austenrerickson@Austens-MacBook-Pro-2.local>
2020-08-16 18:33:38 -0500

    finished writing queries

--- employees_queries.sql
+++ employees_queries.sql
@@ -42,8 +42,60 @@ inner join employees
 	on dept_manager.emp_no = employees.emp_no ;
 
 --List the department of each employee with the following information: employee number, last name, first name, and department name
+select
+	employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+from employees
+inner join dept_emp
+	on employees.emp_no = dept_emp.emp_no
+inner join departments
+	on dept_emp.dept_no = departments.dept_no ;
+
 --List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B"
+select
+	employees.first_name,
+	employees.last_name,
+	employees.sex
+from employees 
+where first_name = 'Hercules' and last_name like 'B%' ;
+
 --List all employees in the Sales department, including their employee number, last name, first name, and department name
+select
+	departments.dept_name,
+	employees.emp_no,
+	employees.last_name,
+	employees.first_name
+from departments
+inner join dept_emp
+	on departments.dept_no = dept_emp.dept_no
+inner join employees
+	on dept_emp.emp_no = employees.emp_no
+where dept_name = 'Sales' ;
+	
+
 --List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name
+
+select
+	departments.dept_name,
+	employees.emp_no,
+	employees.last_name,
+	employees.first_name
+from departments
+inner join dept_emp
+	on departments.dept_no = dept_emp.dept_no
+inner join employees
+	on dept_emp.emp_no = employees.emp_no
+where dept_name = 'Sales' or dept_name = 'Development' ;
+
+
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name
+
+select
+	employees.last_name,
+	count(last_name) as frequency
+from employees 
+group by last_name
+order by count(last_name) desc ;
 
